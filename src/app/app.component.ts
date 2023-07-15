6e6a2ff5aa6b11cca962bb855b11c1aaff8bdf65
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-15 18:00:38 +0200

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Variables
   title = 'gft-fe-gc-angular_componentes_problema4-07-2023';
   datosForm:any[]|undefined;
 
-
+  /* 
+    Metodo para recibir los datos del componente hijo formulario,
+    pasa los datos del evento a una variable que se encuentra en este componente
+  */
   recibirDatos(datos: any) {
     this.datosForm = datos;
   }
--- src/app/formulario-tarea/formulario-tarea.component.ts
+++ src/app/formulario-tarea/formulario-tarea.component.ts
@@ -16,17 +16,20 @@ export class FormularioTareaComponent {
   // Array
   datos:any[]=[];
 
-  // ;ateodo de envia datos para cuando se accione el boton enviar
+  // metodo que envia datos para cuando se accione el boton enviar
   enviarDatos(){
     let complete:boolean=false;
-      
+    
+    // se crea el objeto y se añade al array datos[]
     this.datos.push(
       {
         'titulo' : this.titulo,
         'descripcion' : this.descripcion,
         'complete' : complete
       }
-    )
+    );
+
+    // Una vez añadido el objeto se envia el array por medio del eventemitter
     this.tareaListPush.emit(this.datos);
   }
 
